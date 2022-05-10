c93f0413aa303952882f011be5997e60c5e49cbf
wendelsilva <wendel245.ws@gmail.com>
2022-05-10 15:39:50 -0300

    feat: nlw return backend

---
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -0,0 +1,46 @@
+import { MailAdapter } from "../adapters/mail-adapter";
+import { FeedbacksRepository } from "../repositories/feedbacks-repository";
+
+interface SubmitFeedbackUseCaseRequest {
+    type: string;
+    comment: string;
+    screenshot?: string;
+}
+
+export class SubmitFeedbackUseCase {
+    constructor (
+        private feedbackRepository: FeedbacksRepository,
+        private mailAdapter: MailAdapter,
+    ) {}
+
+    async execute(request: SubmitFeedbackUseCaseRequest) {
+        const {type, comment, screenshot} = request;
+
+        if(!type) {
+            throw new Error('Type is required');
+        }
+
+        if(!comment) {
+            throw new Error('Comment is required');
+        }
+
+        if (screenshot && !screenshot.startsWith('data:image/png;base64,')) {
+            throw new Error('Invalid screenshot format.');
+        }
+
+        await this.feedbackRepository.create({
+            type,
+            comment,
+            screenshot,
+        })
+
+        await this.mailAdapter.sendMail({
+            subject: 'Feedback',
+            body: [
+                `Type: ${type}`,
+                `Comment: ${comment}`,
+                screenshot ? `<img src=${screenshot} />` : ``,
+            ].join('\n'),
+        });
+    }
+}
